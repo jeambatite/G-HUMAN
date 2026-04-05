7e29b3e9117efe284d85a60fd57e57b0c19ce6f3
carlos <junior07122005@gmail.com>
2026-04-05 16:33:05 -0700

    service arreglado

--- proyecto-pasantia/src/app/services/permiso.service.ts
+++ proyecto-pasantia/src/app/services/permiso.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PermisoService {
-  private apiUrl = 'http://localhost:5256/api';
+    private apiUrl = environment.apiUrl;
+
 
   constructor(private http: HttpClient, private authService: AuthService) {}
 
